e95daaa1737aea2a2d787a2a9f008805574824e5
Ran Itzhack <ran.itzhack@gmail.com>
2026-02-25 11:55:11 -0800

    copy the file from your local machine's SSH directory into your Terraform project folder

--- tr_src/modules/ec2/variables.tf
+++ tr_src/modules/ec2/variables.tf
@@ -8,8 +8,14 @@ variable "sg_id" {
   description = "The ID of the security group from the SG module"
 }
 
-# 1. Register the public key with AWS
+# # 1. Register the public key with AWS
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("~/.ssh/cicd_terraform_github_action.pub")
+# }
+
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("~/.ssh/cicd_terraform_github_action.pub")
+  # Note: No "~/", just the filename relative to the terraform files
+  public_key = file("${path.module}/cicd_terraform_github_action.pub")
 }
